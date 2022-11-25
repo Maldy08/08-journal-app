c690b7e0fe1f39942cce79bb41c3ab2e18f03ab7
CARLOS MALDONADO <karloz_0986@hotmail.com>
2022-11-25 15:41:01 -0800

    Finalizando seccion 17

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,33 @@
+import { IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
 
-
-import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       
-      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque mollitia neque numquam officiis velit eligendi impedit? Vero, laudantium suscipit! Repudiandae quae excepturi adipisci quisquam odio eum accusamus ab aliquid quam.</Typography>
-      
+      {/* <Typography>Sint id officia amet velit do aliqua aliqua est ea velit minim voluptate duis laboris. Esse esse consectetur ullamco excepteur ullamco amet. Mollit est nostrud nisi irure magna dolor eiusmod aliquip aliqua nostrud incididunt enim. Velit ipsum laborum Lorem anim laboris aute ullamco ipsum do adipisicing irure.</Typography> */}
+
+      {/* <NothingSelectedView /> */}
+       <NoteView /> 
+
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
+
     </JournalLayout>
   )
 }
